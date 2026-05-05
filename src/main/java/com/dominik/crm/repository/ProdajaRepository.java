5a32a0e39a31a39dadd745d8a8f1bf7733ecb33e
Dominik Duspara <dominik.dusp@gmail.com>
2026-05-05 11:16:30 +0200

    updates in repository queries

--- src/main/java/com/dominik/crm/repository/ProdajaRepository.java
+++ src/main/java/com/dominik/crm/repository/ProdajaRepository.java
@@ -4,10 +4,17 @@ import com.dominik.crm.entity.Prodaja;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.time.LocalDate;
+
 public interface ProdajaRepository extends JpaRepository<Prodaja, Long> {
-    @Query ("SELECT SUM(p.cijena * p.kolicina) FROM Prodaja p WHERE p.kupac.id =  :id")
-    Double ukupnoPoKupcu (@Param("id") Long id);
+    @Query ("SELECT COALESCE (SUM(p.cijena), 0) FROM Prodaja p WHERE p.kupac.id = :id")
+    Double ukupnoPoKupcu(@Param("id") Long id);
+
+    @Query("SELECT COALESCE(SUM(p.cijena), 0) FROM Prodaja p WHERE p.kupac.id = :id AND p.datum BETWEEN :from AND :to")
+    Double ukupnoPoKupcuURasponu(
+            @Param("id") Long id,
+            @Param("from")LocalDate from,
+            @Param("to") LocalDate to
+            );
 }
--- src/main/java/com/dominik/crm/service/ProdajaService.java
+++ src/main/java/com/dominik/crm/service/ProdajaService.java
@@ -4,10 +4,7 @@ import com.dominik.crm.entity.Artikl;
 import com.dominik.crm.repository.ArtiklRepository;
 import com.dominik.crm.repository.ProdajaRepository;
 import com.dominik.crm.entity.Prodaja;
-import org.springframework.boot.autoconfigure.context.LifecycleAutoConfiguration;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.List;
 
@@ -31,25 +28,15 @@ public class ProdajaService {
         prodaja.setCijena(ukupno);
         return prodajaRepository.save(prodaja);
     }
-;    public List<Prodaja> sveProdaje(){
+    public List<Prodaja> sveProdaje(){
 
         return prodajaRepository.findAll();
     }
     public Double ukupnoPoKupcu (Long kupacId) {
-        List<Prodaja> prodaje = prodajaRepository.findAll();
-        return prodaje.stream()
-                .filter(p -> p.getKupac().getId().equals(kupacId))
-                .mapToDouble(Prodaja::getCijena)
-                .sum();
+        return prodajaRepository.ukupnoPoKupcu(kupacId);
     }
 
-    public double ukupnoPoKupcuURasponu (Long kupacId, LocalDate from, LocalDate to){
-        List<Prodaja> prodaje   = prodajaRepository.findAll();
-        return prodaje.stream()
-                .filter(p-> p.getKupac().getId().equals(kupacId))
-                .filter(p-> p.getDatum().isAfter(from) || p.getDatum().isEqual(from))
-                .filter(p->p.getDatum().isBefore(to) || p.getDatum().isEqual(to))
-                .mapToDouble(Prodaja::getCijena)
-                .sum();
+    public Double ukupnoPoKupcuURasponu (Long kupacId, LocalDate from, LocalDate to){
+        return prodajaRepository.ukupnoPoKupcuURasponu(kupacId, from, to);
     }
 }
