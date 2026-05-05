f887814c2ef32c76fc73c5e6f6f9ce06bbf5d406
Dominik Duspara <dominik.dusp@gmail.com>
2026-05-05 11:50:21 +0200

    updated ProdajaService

--- src/main/java/com/dominik/crm/service/ProdajaService.java
+++ src/main/java/com/dominik/crm/service/ProdajaService.java
@@ -1,7 +1,9 @@
 package com.dominik.crm.service;
 
 import com.dominik.crm.entity.Artikl;
+import com.dominik.crm.entity.Kupac;
 import com.dominik.crm.repository.ArtiklRepository;
+import com.dominik.crm.repository.KupacRepository;
 import com.dominik.crm.repository.ProdajaRepository;
 import com.dominik.crm.entity.Prodaja;
 import org.springframework.stereotype.Service;
@@ -13,17 +15,25 @@ public class ProdajaService {
 
     private final ProdajaRepository prodajaRepository;
     private final ArtiklRepository artiklRepository;
+    private final KupacRepository kupacRepository;
 
-    public ProdajaService(ProdajaRepository prodajaRepository, ArtiklRepository artiklRepository) {
+    public ProdajaService(ProdajaRepository prodajaRepository, ArtiklRepository artiklRepository, KupacRepository kupacRepository) {
 
         this.prodajaRepository = prodajaRepository;
         this.artiklRepository = artiklRepository;
+        this.kupacRepository = kupacRepository;
     }
     public Prodaja spremi (Prodaja prodaja){
+        Kupac kupac = kupacRepository
+                .findById(prodaja.getKupac().getId())
+                .orElseThrow(()-> new RuntimeException("Kupac ne postoji"));
+
         Artikl artikl = artiklRepository
                 .findById(prodaja.getArtikl().getId())
                 .orElseThrow(() -> new RuntimeException("Artikl ne postoji"));
         double ukupno = artikl.getCijena() * prodaja.getKolicina();
+
+        prodaja.setKupac(kupac);
         prodaja.setArtikl(artikl);
         prodaja.setCijena(ukupno);
         return prodajaRepository.save(prodaja);
