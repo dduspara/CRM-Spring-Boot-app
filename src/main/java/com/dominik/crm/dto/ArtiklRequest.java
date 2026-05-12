78936f3be6843037cf067d9ffc752a88cf6d2668
Dominik Duspara <dominik.dusp@gmail.com>
2026-05-12 10:57:23 +0200

    .

--- src/main/java/com/dominik/crm/dto/ArtiklRequest.java
+++ src/main/java/com/dominik/crm/dto/ArtiklRequest.java
@@ -3,11 +3,13 @@ package com.dominik.crm.dto;
 import com.dominik.crm.entity.TipArtikla;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public class ArtiklRequest {
     @NotBlank(message = "Naziv artikla je obavezan.")
     private String naziv;
     @NotNull(message = "Cijena je obavezna.")
+    @Positive(message = "Cijena mora biti veca od 0.")
     private Double cijena;
     @NotNull(message = "Tip artikla je obavezan.")
     private TipArtikla tip;
